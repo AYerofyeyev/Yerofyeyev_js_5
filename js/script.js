e460f098b2af9efb6e1f404019f94bd860bdaba6
AYerofyeyev <a.yerofyeyev@gmail.com>
2017-02-14 14:33:14 +0200

    v.1

--- js/script.js
+++ js/script.js
@@ -1,17 +1,42 @@
-var zeroed = "";
+var watch = "";
+var ms = 0;
+var s = 0;
+var m = 0;
+var i;
 
-
-function startStopwatch() {
-
-
+function timer() {
+  ms++;
+  if (ms > 99) {
+    s++;
+    ms = 0;
+  }
+  if (s > 59) {
+    m++;
+    s = 0;
+  }
+  watch = m + ":" + s + "." + ms;
+  clockface.value = watch;
+  i = setTimeout("timer()", 1);
 }
 
+function startStopwatch() {
+  swapStart();
+  timer();
+}
 
+function stopStopwatch() {
+  swapStop();
+  clearTimeout(i);
+  console.log("s =" + s);
+}
 
 function clearStopwatch() {
-  clearTimeout();
-  zeroed = "00:00.000";
-  clockface.value = zeroed;
+  clearTimeout(i);
+  m = 0;
+  s = 0;
+  ms = 0;
+  watch = "00:00.00";
+  clockface.value = watch;
 }
 
 function swapStart() {
@@ -31,7 +56,7 @@ form.style.width = "800px";
 form.style.border = "2px solid #000";
 form.style.borderRadius = "10px";
 form.style.overflow = "hidden";
-form.style.padding = "20px";
+form.style.padding = "50px";
 form.style.margin = "50px auto";
 
 var header = document.createElement("h1");
@@ -40,9 +65,10 @@ header.style.textAlign = "center"
 
 var clockface = document.createElement("input");
 clockface.name = "clockface";
-clockface.value = "00:00.000";
+clockface.value = "00:00.00";
 clockface.style.fontSize = "30px";
 clockface.style.textAlign = "center";
+clockface.style.float = "left";
 clockface.style.marginLeft = "198px";
 
 var startButton = document.createElement("button");
@@ -68,10 +94,10 @@ clearButton.style.width = "200px";
 
 document.body.appendChild(form);
 form.appendChild(header);
+form.appendChild(clockface);
 form.appendChild(startButton);
 form.appendChild(clearButton);
-form.appendChild(clockface);
 
-startButton.addEventListener("click", swapStart);
-stopButton.addEventListener("click", swapStop);
+startButton.addEventListener("click", startStopwatch);
+stopButton.addEventListener("click", stopStopwatch);
 clearButton.addEventListener("click", clearStopwatch);
