5458ed5eb282ecdf311938b97bdd27597d9745d9
AYerofyeyev <a.yerofyeyev@gmail.com>
2017-02-15 12:02:51 +0200

    checkpoints're added

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 var watch = "";
+var v = "";
 var ms = 0;
 var s = 0;
 var m = 0;
-var i;
+var i = 0;
+var t;
+var check = [];
 
 function timer() {
   ms++;
@@ -16,7 +19,7 @@ function timer() {
   }
   watch = m + ":" + s + "." + ms;
   clockface.value = watch;
-  i = setTimeout("timer()", 1);
+  t = setTimeout("timer()", 1);
 }
 
 function startStopwatch() {
@@ -26,26 +29,39 @@ function startStopwatch() {
 
 function stopStopwatch() {
   swapStop();
-  clearTimeout(i);
+  clearTimeout(t);
 }
 
 function clearStopwatch() {
-  clearTimeout(i);
+  clearTimeout(t);
   m = 0;
   s = 0;
   ms = 0;
+  i = 0;
+  check = [];
   watch = "00:00.00";
   clockface.value = watch;
+  textarea.value = null;
+  textarea.rows = 1;
 }
 
 function swapStart() {
   form.removeChild(startButton);
-  form.appendChild(stopButton);
+  form.insertBefore(stopButton, checkButton);
 }
 
 function swapStop() {
   form.removeChild(stopButton);
-  form.appendChild(startButton);
+  form.insertBefore(startButton, checkButton);
+}
+
+function round() {
+  check.push (watch);
+  console.log(check[i]);
+  v = (i + 1) + ". " + check[i] + "\n";
+  i++;
+  textarea.value += v;
+  textarea.rows = (i < 10) ? i : 10;
 }
 
 var form = document.createElement("form");
@@ -57,6 +73,8 @@ form.style.borderRadius = "10px";
 form.style.overflow = "hidden";
 form.style.padding = "50px";
 form.style.margin = "50px auto";
+form.style.textAlign = "center";
+form.style.position = "relative";
 
 var header = document.createElement("h1");
 header.innerHTML = "СЕКУНДОМЕР";
@@ -67,22 +85,27 @@ clockface.name = "clockface";
 clockface.value = "00:00.00";
 clockface.style.fontSize = "30px";
 clockface.style.textAlign = "center";
-clockface.style.float = "left";
-clockface.style.marginLeft = "198px";
+clockface.style.margin = "50px 200px";
 
 var startButton = document.createElement("button");
 startButton.type = "button";
 startButton.className = "button-success pure-button";
 startButton.innerHTML = "СТАРТ";
-startButton.style.float = "left";
 startButton.style.width = "200px";
 
 var stopButton = document.createElement("button");
 stopButton.type = "button";
 stopButton.className = "button-error pure-button";
 stopButton.innerHTML = "СТОП";
-stopButton.style.float = "left";
 stopButton.style.width = "200px";
+
+var checkButton = document.createElement("button");
+checkButton.type = "button";
+checkButton.className = "button-secondary pure-button";
+checkButton.innerHTML = "ЧЕКПОИНТ";
+checkButton.style.width = "200px";
+checkButton.style.float = "right";
+checkButton.style.marginRight = "100px";
 
 var clearButton = document.createElement("button");
 clearButton.type = "button";
@@ -90,13 +113,24 @@ clearButton.className = "button-warning pure-button";
 clearButton.innerHTML = "ОБНУЛИТЬ";
 clearButton.style.float = "right";
 clearButton.style.width = "200px";
+clearButton.style.overflow = "hidden";
+
+var textarea = document.createElement("textarea");
+textarea.className = "pure-input-1-2";
+textarea.style.display = "inline-block";
+textarea.style.marginTop = "50px";
+textarea.style.width = "200px";
+textarea.rows = 1;
 
 document.body.appendChild(form);
 form.appendChild(header);
 form.appendChild(clockface);
-form.appendChild(startButton);
 form.appendChild(clearButton);
+form.appendChild(checkButton);
+form.appendChild(startButton);
+form.appendChild(textarea);
 
 startButton.addEventListener("click", startStopwatch);
 stopButton.addEventListener("click", stopStopwatch);
 clearButton.addEventListener("click", clearStopwatch);
+checkButton.addEventListener("click", round);
